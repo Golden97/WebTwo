c886e5422cff1c7b075a2858d0d6cd6d225153f3
Golden97 <andreaplu07@gmail.com>
2019-09-16 11:33:04 +0200

    hfhf

--- src/main/java/com/persistance/SuperAdminJPA.java
+++ src/main/java/com/persistance/SuperAdminJPA.java
@@ -1,9 +1,21 @@
 package com.persistance;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 
 public class SuperAdminJPA {
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
